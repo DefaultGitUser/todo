22e417131340999403d70789b6d625036f6fc790
Oleg Ilnytskyy <ilnytskyy67922@gmail.com>
2023-05-29 16:21:52 +0300

    +filter

--- js/app.js
+++ js/app.js
@@ -3,15 +3,71 @@
 const root = document.querySelector('.todoapp');
 const newTodoField = root.querySelector('.new-todo');
 const itemList = root.querySelector('.todo-list');
+const allToggler = root.querySelector('#toggle-all');
+const clearCompleetedButton = root.querySelector('.clear-completed');
+const filter = root.querySelector('.filters');
+
+filter.addEventListener('click', (e) => {
+  if (!e.target.dataset.filter) {
+    return;
+  }
+
+  const togglers = root.querySelectorAll('.toggle');
+
+  const filterButtons = root.querySelectorAll('[data-filter]'); 
+
+  for (const button of filterButtons) {
+    button.classList.toggle('selected', button === e.target);
+  }
+
+  for (const toggler of togglers) {
+    const item = toggler.closest('.todo-item');
+
+    switch (e.target.dataset.filter) {
+      case 'all':
+        item.hidden = false;
+        break;
+      case 'active':
+        item.hidden = toggler.checked;
+        break;
+      case 'completed':
+        item.hidden = !toggler.checked;
+        break;
+    }
+  }
+});
 
 function updateInfo() {
   const counter = root.querySelector('.todo-count');
-  
+  const completedTogglers = root.querySelectorAll('.toggle:checked');
   const nonCompletedTogglers = root.querySelectorAll('.toggle:not(:checked)')
   
   counter.innerHTML = `${nonCompletedTogglers.length} items left`;
+  allToggler.checked = !nonCompletedTogglers.length;
+  clearCompleetedButton.hidden = !completedTogglers.length;
 }
-// console.log(root);
+
+clearCompleetedButton.addEventListener('click', () => {
+  const completedTogglers = root.querySelectorAll('.toggle:checked');
+
+  for (const toggle of completedTogglers) { 
+    toggle.closest('.todo-item').remove();
+  }
+
+  updateInfo();
+});
+
+allToggler.addEventListener('change', (e) => {
+  const togglers = root.querySelectorAll('.toggle'); 
+
+  for (const toggle of togglers) {
+    toggle.checked = allToggler.checked;
+    toggle.closest('.todo-item').classList.toggle('completed', allToggler.checked);
+  }
+
+  updateInfo();
+});
+
 newTodoField.addEventListener('keydown', (e) => {
   if (e.key !== 'Enter') {
     return;
